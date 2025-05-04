a0e595cd33cfc53a87b4abba86c0ede596146249
linke <linke@duck.com>
2025-05-04 23:09:51 +0300

    Task7

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
     task6();
+    task7();
     }
     public static void  task6() {
         var boxerWeight1 = 78.2;
@@ -9,5 +10,13 @@ public class Main {
         var weightDifference = boxerWeight2 - boxerWeight1;
         System.out.println("Общий вес двух боксеров равен " + totalWeight + " кг");
         System.out.println("Разница между боксерами составляет " + weightDifference + " кг");
+    }
+    public static void task7() {
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var weightDifference1 = boxerWeight2 - boxerWeight1;
+        var weightDifference2 = boxerWeight2 % boxerWeight1;
+        System.out.println("Разница между боксерами составляет " + weightDifference1 + " кг");
+        System.out.println("Разница между боксерами составляет " + weightDifference2 + " кг");
     }
 }
